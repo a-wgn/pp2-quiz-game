16c778d41149bfa669bd4750f6b06660b4da8d87
a-wgn <antonwgn1993@gmail.com>
2022-12-18 12:41:42 +0000

    Add function to show questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,40 @@
 const playButton = document.getElementById('start-container')
 const questionContainer = document.getElementById('question-container')
 
+const questionElement = document.getElementById('question')
+const awButton = document.getElementById('answer-button')
+
+let shuffledQuestions, currentQuestionIndex
+
 playButton.addEventListener('click', playGame)
 
 function playGame() {
     console.log('Started')
     playButton.classList.add('hide')
+    shuffledQuestions = question.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainer.classList.remove('hide')
     nextQuestion()
 }
 
+function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+const question = [
+    {
+        question: 'Whats my name?',
+        answers: [
+            { text: 'Anton', correct: true }, 
+            { text: 'Daniel', correct: false }
+        ]
+    }
+]
